d58b8222612a95bc891944ea3232bb868f8870c4
Luciano Montezzo <lmontezzo@gmail.com>
2017-04-12 11:53:49 -0300

    restaurant model done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  # get 'restaurants/index'
+
+  # get 'restaurants/new'
+
+  # get 'restaurants/edit'
+
+  # get 'restaurants/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants
 end
